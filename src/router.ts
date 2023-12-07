fb4563c7a50c79d8f5b737b50e94d0e86903e6b8
smart-builder <ringroseleekevin@gmail.com>
2023-12-07 19:26:14 +0000

    commit use web-hash-history for deployment

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from 'vue-router';
+import { createWebHashHistory, createRouter } from 'vue-router';
 import HomeDashboard from './views/HomeDashboard.vue';
 import DigitalDivide from './views/DigitalDivide.vue';
 import WhatWeDo from './views/WhatWeDo.vue';
@@ -48,7 +48,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/zh-good-things-foundation/'),
   routes
 });
 
